5690a10fd717fe4aac72bf1e9cf9b33e688f4491
ratulkhan44 <ratulcse.khan@gmail.com>
2020-11-22 11:21:11 +0600

    slider project complete

--- src/pages/Slider.vue
+++ src/pages/Slider.vue
@@ -1,12 +1,38 @@
 <template>
-  <div class="flex flex-wrap w-full">
-    <div class="absolute w-full" v-for="(color, index) in sliders" :key="color">
-      <div
-        v-if="currentSlide == index"
-        :class="color"
-        style="height: 350px"
-      ></div>
+  <div class="flex flex-wrap w-full relative">
+    <div class="w-full absolute" v-for="(color, index) in sliders" :key="color">
+      <transition name="fade">
+        <div
+          v-if="currentSlide == index"
+          :class="color"
+          style="height: 350px"
+        ></div>
+      </transition>
     </div>
+    <div class="w-full" style="height: 340px">
+      <div class="absolute bottom-0 w-full flex justify-center">
+        <div
+          v-for="(slider, index) in sliders"
+          :key="slider"
+          @click="makeActive(index)"
+          class="w-4 h-4 mx-2 rounded-full cursor-pointer shadow-md"
+          :class="currentSlide == index ? 'bg-gray-700' : 'bg-gray-300'"
+        ></div>
+      </div>
+    </div>
+    <!-- <div class="w-full my-10 flex">
+      <div class="m-auto">
+        <transition name="fade">
+          <h1 v-if="isTitleShowing">Slider Carousel</h1>
+        </transition>
+        <button
+          @click="isTitleShowing = !isTitleShowing"
+          class="px-2 rounded border"
+        >
+          Toggle Text
+        </button>
+      </div>
+    </div> -->
   </div>
 </template>
 
@@ -17,13 +43,19 @@ export default {
       currentSlide: 0,
       interval: "",
       sliders: ["bg-blue-600", "bg-yellow-600", "bg-teal-600"],
+      isTitleShowing: true,
     };
+  },
+  methods: {
+    makeActive(index) {
+      this.currentSlide = index;
+    },
   },
   mounted() {
     this.interval = setInterval(() => {
       this.currentSlide = this.currentSlide == 2 ? 0 : this.currentSlide + 1;
       console.log(this.currentSlide);
-    }, 1000);
+    }, 2000);
   },
   beforeUnmount() {
     clearInterval(this.interval);
@@ -32,4 +64,16 @@ export default {
 </script>
 
 <style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 1s ease;
+}
+.fade-enter-from {
+  opacity: 0;
+  transform: translateX(-100%);
+}
+.fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateX(100%);
+}
 </style>
